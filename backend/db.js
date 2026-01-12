86728387984d07809255232e55d31925ba86a6fa
chris_na <lamichhane2060@gmail.com>
2026-01-12 14:49:55 +0545

    Fixed Secrets

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://lamichhane2060_db_user:9y2vZ0XfmKRjxXy8@blogapp.8ynujdh.mongodb.net/?appName=blogapp")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
